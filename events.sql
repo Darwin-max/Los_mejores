587f0c166fad124669731dc947e7515effdb83be
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-07-04 09:50:13 -0500

    Eventos Juan david
    * feat: :sparkles: Actualizar pagos pendientes a 'Vencido' si tienen más de un año de antigüedad
    
    * feat: :sparkles: -- 12. Generar reporte mensual de cuotas de manejo
    
    * feat: :sparkles: -- 14. Desactivar tarjetas sin movimiento en 6 meses
    
    * feat: :sparkles: -- 15. Activar tarjetas con movimiento reciente en 6 meses
    
    * feat: :sparkles: -- 18. Verificar y ajustar montos negativos

---
+++ events.sql
@@ -0,0 +1,101 @@
+-- este son los mejores eventos de juan david 
+
+
+-- 8. Actualizar pagos pendientes a 'Vencido' si tienen más de un año de antigüedad
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_actualizar_pagos_pendientes_antiguos $$
+
+CREATE EVENT evt_actualizar_pagos_pendientes_antiguos
+ON SCHEDULE EVERY 1 DAY
+ENABLE
+DO
+BEGIN
+    UPDATE pago SET estado = 'Vencido'
+    WHERE estado = 'Pendiente' AND
+    fecha < DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+
+END $$
+
+DELIMITER ;
+
+
+-- 12. Generar reporte mensual de cuotas de manejo
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_generar_reporte_cuotas_manejo  $$
+
+CREATE EVENT evt_generar_reporte_cuotas_manejo
+ON SCHEDULE EVERY 1 MONTH STARTS CURRENT_DATE + INTERVAL 1 MONT
+ENABLE
+DO
+BEGIN
+    INSERT INTO reporte_cuotas_manejo (fecha_generacion, total_cuotas)
+    SELECT NOW(), SUM(total_monto) FROM cuota_manejo;
+
+END $$
+
+DELIMITER ;
+
+-- 14. Desactivar tarjetas sin movimiento en 6 meses
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_desactivar_tarjetas_inactivas $$
+
+CREATE EVENT evt_desactivar_tarjetas_inactivas
+ON SCHEDULE EVERY 1 MONTH
+ENABLE
+DO
+BEGIN
+    UPDATE tarjeta
+    SET estado = 'Inactiva'
+    WHERE id NOT IN (
+        SELECT DISTINCT tarjeta_id FROM pago
+        WHERE fecha > DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
+    ) AND estado = 'Activa';
+
+END $$
+
+DELIMITER ;
+
+
+-- 15. Activar tarjetas con movimiento reciente en 6 meses
+
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_activar_tarjetas_activas_recientes $$
+
+CREATE EVENT evt_activar_tarjetas_activas_recientes
+ON SCHEDULE EVERY 1 MONTH
+ENABLE
+DO
+BEGIN
+    UPDATE tarjeta
+    SET estado = 'Activa'
+    WHERE id IN (
+        SELECT DISTINCT tarjeta_id FROM pago
+        WHERE fecha > DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
+    )
+    AND estado = 'Inactiva';
+END $$
+
+DELIMITER ;
+
+
+-- 18. Verificar y ajustar montos negativos
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_ajuste_montos_negativos $$
+
+CREATE EVENT evt_ajuste_montos_negativos
+ON SCHEDULE EVERY 1 WEEK
+ENABLE
+DO
+BEGIN
+    UPDATE cuenta_bancaria
+    SET monto = 0, estado = 'Bloqueada'
+    WHERE monto < 0;
+
+END $$
+
+DELIMITER ;
