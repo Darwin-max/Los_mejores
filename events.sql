7e683ab5f5fd23f995d206bfde074a7c94a285e5
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-07-04 09:42:12 -0500

    feat: :sparkles: Actualizar pagos pendientes a 'Vencido' si tienen más de un año de antigüedad

---
+++ events.sql
@@ -0,0 +1,21 @@
+-- este son los mejores eventos de juan david 
+
+
+-- 8. Actualizar pagos pendientes a 'Vencido' si tienen más de un año de antigüedad
+DELIMITER $$
+
+DROP EVENT IF EXISTS evt_actualizar_pagos_pendientes_antiguos $$
+
+CREATE EVENT evt_actualizar_pagos_pendientes_antiguos
+ON SCHEDULE EVERY 1 DAY
+ENABLE
+DO
+BEGIN
+    UPDATE pago SET estado = 'Vencido'
+    WHERE estado = 'Pendiente' AND
+    fecha < DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+
+END $$
+
+DELIMITER ;
+
