527209a927b825a58382390e8b7fce07b798e0cb
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-07-04 11:51:24 -0500

    eventos de santigo

--- events.sql
+++ events.sql
@@ -251,3 +251,92 @@ BEGIN
 END;
 
 SHOW EVENTS;
+
+
+
+-- eventos de santigo
+
+
+-- Insertar seguridad por defecto en tarjetas sin PIN
+
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_insertar_pin_defecto;
+CREATE EVENT IF NOT EXISTS ev_insertar_pin_defecto
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    INSERT INTO Seguridad_tarjetas (tarjeta_id, pin)
+    SELECT t.id, '1234'
+    FROM Tarjetas t
+    LEFT JOIN Seguridad_tarjetas s ON t.id = s.tarjeta_id
+    WHERE s.id IS NULL;
+END //
+
+DELIMITER ;
+
+
+-- Evento para actualizar el estado de las tarjetas vencidas
+
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_actualizar_tarjetas_vencidas;
+CREATE EVENT IF NOT EXISTS ev_actualizar_tarjetas_vencidas
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    UPDATE Tarjetas
+    SET estado = 'Vencida'
+    WHERE fecha_expiracion < CURDATE() AND estado <> 'Vencida';
+END //
+
+DELIMITER ;
+
+
+--  Cambiar automaticamente el estado de las cuotas de manejo si no se han pagado
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_actualizar_estado_cuotas_manejo;
+CREATE EVENT IF NOT EXISTS ev_actualizar_estado_cuotas_manejo
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    UPDATE Cuotas_de_manejo
+    SET estado = 'Pendiente'
+    WHERE vencimiento_cuota < CURDATE() AND estado <> 'Pago';
+END //
+
+DELIMITER ;
+
+
+-- Cambiar estado a 'Bloqueada' si la tarjeta tiene saldo negativo
+
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_bloquear_tarjetas_saldo_negativo;
+CREATE EVENT IF NOT EXISTS ev_bloquear_tarjetas_saldo_negativo
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    UPDATE Tarjetas
+    SET estado = 'Bloqueada'
+    WHERE saldo < 0 AND estado = 'Activa';
+END //
+
+DELIMITER ;
+
+
+-- Eliminar historial de pagos con estado 'Inicio' mayor a 1 año
+
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_limpiar_historial_inicial;
+CREATE EVENT IF NOT EXISTS ev_limpiar_historial_inicial
+ON SCHEDULE EVERY 1 DAY
+DO
+BEGIN
+    DELETE FROM Historial_de_pagos
+    WHERE estado_anterior = 'Inicio' AND fecha_cambio < CURDATE() - INTERVAL 1 YEAR;
+END //
+
+DELIMITER ;
